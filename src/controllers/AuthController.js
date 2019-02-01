e28e7fe1ae0f54664faa9816da904fb15de97008
Bruno Heinrich <bruno.heinrich@superlogica.com>
2019-02-01 18:23:08 -0200

    Adicionando regras básicas para validação do token

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -6,32 +6,15 @@ module.exports = {
   async auth(req, res) {
 
     jwt.verify(req.headers.token, config.secret, function(err, decoded) {
-      // console.log(decoded);
-
-      if (decoded.email !== req.headers.email) {
-        return res.json("You shall not pass");
+      if (err) {
+        return res.json("false time");
       }
 
-      // GERA UM NOVO TIMESTAMP
-      const timestamp = new Date().getTime();
-      // console.log(timestamp);
-      // console.log(decoded.exp);
-      // console.log(timestamp - decoded.exp);
-      // var TimeStamp = timestamp - decoded.exp;
+      if (decoded.email !== req.headers.email) {
+        return res.json("false email");
+      }
 
-      // CONVERTE TIMESTAMP PARA DATA
-      const teste = new Date(timestamp*1000);
-
-      console.log(teste.toLocaleDateString("pt-BR"));
-      console.log(teste);
-      // console.log(timestamp.toLocaleDateString("pt-BR"));
-      // console.log(timestamp);
-
-      // if (res.status(500)) {
-      //   return ({ auth: false, message: 'Failed to authenticate token.' });
-      // }
-
-      return res.json("true");
+      return res.json("True");
     });
   },
 }

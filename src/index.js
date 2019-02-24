62378d3bf847adf9db3f8da2ad2952492f67f001
Bruno Heinrich <bruno.heinrich@superlogica.com>
2019-02-24 20:19:36 -0300

    Implementando estrutura para testes automaticos

--- src/index.js
+++ src/index.js
@@ -10,6 +10,9 @@ const io = require('socket.io')(server);
 mongoose.connect('mongodb://root:root123@ds211724.mlab.com:11724/calendario-evento', {
   useNewUrlParser: true
 });
+// mongoose.connect('mongodb://root:root123@ds331145.mlab.com:31145/eventstest', {
+  // useNewUrlParser: true
+// });
 
 app.use((req, res, next) => {
   req.io = io;

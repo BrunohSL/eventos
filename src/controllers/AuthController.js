35984440bbc32d20359ee9666bed87dc4215fe09
Bruno Heinrich <bruno.heinrich@superlogica.com>
2019-05-09 11:24:32 -0300

    documentação

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -2,18 +2,24 @@ jwt = require('jsonwebtoken');
 const config = require('../../config');
 
 module.exports = {
+    /**
+     * Função responsável por autenticar o token de login
+     */
+    async auth(req, res, next) {
+        // Função que verifica o token enviado
+        jwt.verify(req.headers.token, config.secret, function(err, decoded) {
+        // Trata a mensagem de erro
+        if (err) {
+            return res.json(err);
+        }
 
-  async auth(req, res, next) {
-    jwt.verify(req.headers.token, config.secret, function(err, decoded) {
-      if (err) {
-        return res.json(err);
-      }
-
-      if (decoded.email !== req.headers.email) {
-        return res.json("You shall not pass");
-      }
-      console.log("Valid authentication");
-      next();
-    });
-  },
+        // Caso o e-mail enviado no header não seja igual ao e-mail do token
+        if (decoded.email !== req.headers.email) {
+            return res.json("You shall not pass");
+        }
+        console.log("Valid authentication");
+        // Chama a próxima função após realizar a validação
+        next();
+        });
+    },
 }
